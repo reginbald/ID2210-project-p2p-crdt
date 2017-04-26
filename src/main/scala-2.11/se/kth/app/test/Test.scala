cc48a4c0455af0c0b31528f4c88faad1e0e2741b
reginbald <mail@reginbald.com>
2017-04-26 13:27:06 +0200

    Ping pong class update

--- src/main/scala-2.11/se/kth/app/test/Test.scala
+++ src/main/scala-2.11/se/kth/app/test/Test.scala
@@ -5,6 +5,5 @@ import se.sics.kompics.KompicsEvent
 /**
   * Created by reginbald on 26/04/2017.
   */
-class Pong extends KompicsEvent{
-
-}
+case class Ping extends KompicsEvent
+case class Pong extends KompicsEvent
