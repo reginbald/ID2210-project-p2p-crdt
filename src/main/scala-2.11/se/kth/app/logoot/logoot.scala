936335c2e6bb4af78d9c01f0dcb7c3a1330aaaeb
einarlogi <einarlogi14@gmail.com>
2017-05-17 19:14:03 +0200

    logoot initial

--- src/main/scala-2.11/se/kth/app/logoot/logoot.scala
+++ src/main/scala-2.11/se/kth/app/logoot/logoot.scala
@@ -1,11 +1,20 @@
 package se.kth.app.logoot
 
 import com.typesafe.scalalogging.StrictLogging
-import se.sics.kompics.sl.ComponentDefinition
+import se.sics.kompics.{KompicsEvent, Start}
+import se.sics.kompics.sl.{ComponentDefinition, handle}
+
+import scala.collection.mutable.Set
 
 class logoot extends ComponentDefinition with StrictLogging {
 
   var identifierTable : List[(Int, Int, Int)] = List.empty
-  
+
+  /* Logoot events */
+  ctrl uponEvent {
+    case _:Start => handle {
+      logger.info("--- Logoot is starting ---")
+    }
+  }
 
 }
