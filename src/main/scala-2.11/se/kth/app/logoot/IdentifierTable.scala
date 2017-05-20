178fafc0ff6e2502bbdb9c04eed18d3bf9dae783
reginbald <mail@reginbald.com>
2017-05-20 15:44:42 +0200

    Identifier table

---
+++ src/main/scala-2.11/se/kth/app/logoot/IdentifierTable.scala
@@ -0,0 +1,32 @@
+package se.kth.app.logoot
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by reginbald on 20/05/2017.
+  */
+class IdentifierTable {
+  private var table:ListBuffer[LineId] = new ListBuffer[LineId]
+
+  def binarySearch(key: LineId): Int = {
+    var out: Int = -1
+    var lo: Int = 0
+    var hi: Int = table.size - 1
+    while (lo <= hi) {
+      // Key is in a[lo..hi] or not present.
+      val mid = lo + (hi - lo) / 2
+      if (key.lessThan(table(mid))) hi = mid - 1
+      else if (table(mid).lessThan(key)) lo = mid + 1
+      else out = mid
+    }
+    out
+  }
+
+  def insert(index: Int, id: LineId) = {
+    table.insert(index, id)
+  }
+
+  def remove(index: Int, id: LineId) = {
+    table.remove(index)
+  }
+}
--- src/main/scala-2.11/se/kth/app/logoot/LineId.scala
+++ src/main/scala-2.11/se/kth/app/logoot/LineId.scala
@@ -1,10 +1,33 @@
 package se.kth.app.logoot
 
 import scala.collection.mutable
+import scala.util.control.Breaks._
 
 /**
   * Created by reginbald on 17/05/2017.
   */
 class LineId(val positions: mutable.ListBuffer[Position]) {
+  def lessThan(that: LineId): Boolean = {
+    var out:Boolean = false;
+
+    for (i <- 0 to positions.size -1){
+      if (positions(i).digit < that.positions(i).digit) {
+        out = true
+        break
+      }
+      else if (positions(i).digit == that.positions(i).digit &&
+        positions(i).siteId.getId.asInstanceOf[Int] < that.positions(i).siteId.getId.asInstanceOf[Int]){
+        out = true
+        break
+      }
+      else if (positions(i).digit == that.positions(i).digit &&
+        positions(i).siteId.getId.asInstanceOf[Int] == that.positions(i).siteId.getId.asInstanceOf[Int] &&
+        positions(i).clock < that.positions(i).clock) {
+        out = true
+        break
+      }
+    }
+    out
+  }
 
 }
