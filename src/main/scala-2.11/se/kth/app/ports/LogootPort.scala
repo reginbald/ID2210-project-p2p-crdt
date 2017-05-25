5b1fd507d45be980d31276ca5d1ffb5c74c1a308
reginbald <mail@reginbald.com>
2017-05-25 16:04:12 +0200

    logoot port can take in undo redo events

--- src/main/scala-2.11/se/kth/app/ports/LogootPort.scala
+++ src/main/scala-2.11/se/kth/app/ports/LogootPort.scala
@@ -1,10 +1,12 @@
 package se.kth.app.ports
 
-import se.kth.app.events.{Logoot_Do, Logoot_Doc, Logoot_Done, Logoot_Patch}
+import se.kth.app.events._
 import se.sics.kompics.sl.Port
 
 class LogootPort extends Port {
   request[Logoot_Do] // in
+  request[Logoot_Undo] // in
+  request[Logoot_Redo] // in
   indication[Logoot_Doc] // out
   indication[Logoot_Done] // out
   indication[Logoot_Patch] // out
